578f00a62739fff0898bc8783dcf611ce027c2bc
Sharmayne <sharmayne_lim@mymail.sutd.edu.sg>
2023-03-23 16:58:43 +0800

    wip

--- graph.js
+++ graph.js
@@ -1,5 +1,14 @@
 // Load the data
-d3.csv("jan2017onwards_edited.csv", function(data) {
+d3.csv("https://raw.githubusercontent.com/sl194/HASS-assignment3/main/jan2017onwards_edited.csv?token=GHSAT0AAAAAAB7LT4EK7JD6ENMKB2UQT5JWZA4C7TA", function(d) {
+  return {
+    town: d.town,
+    resale_price: +d.resale_price,
+    flat_type: d.flat_type,
+    floor_area_sqm: +d.floor_area_sqm,
+  };
+}).then(function(data) {
+        const width = window.innerWidth-10;
+    const height = window.innerHeight;
 // Select the buttons
 const button1 = d3.select("#room-1");
 const button2 = d3.select("#room-2");
@@ -7,7 +16,7 @@ const button3 = d3.select("#room-3");
 const button4 = d3.select("#room-4");
 const button5 = d3.select("#room-5");
 const buttonExec = d3.select("#room-exec");
-
+   
 // Add event listeners to each button
 button1.on("click", function() {
   updateGraph("1 ROOM");
@@ -32,11 +41,37 @@ buttonExec.on("click", function() {
     console.log(Array.isArray(data));
 // Define a function to update the graph based on the selected unit type
 function updateGraph(unitType) {
-  // Filter the data by the selected unit type
-  const filteredData = data.filter(function(d) {
-    return d.flat_type === unitType;
-  });
+     // Filter the data for the selected unit type
+    const filteredData = data.filter(d => d["flat_type"] === unitType);
 
+    // Group the data by town and unit type
+    const townData = d3.group(filteredData, d => d.town + "-" + d["flat_type"]);
+
+    // Calculate the average price per square meter for each town and unit type
+    const townPrices = Array.from(townData, ([town, data]) => {
+      const pricePerSqM = d3.mean(data, d => d["resale_price"] / d["floor_area_sqm"]);
+      return { town: town.split("-")[0], unitType: town.split("-")[1], pricePerSqM };
+    }).filter(d => d.unitType === unitType);
+
+    // Sort the data by price per square meter
+    townPrices.sort(function(a, b) {
+      return a.pricePerSqM - b.pricePerSqM;
+    });
+
+      const xScale = d3.scaleBand()
+    .domain(townPrices.map(d => d.town))
+    .range([0, width])
+    .padding(0.1);
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(townPrices, d => d.pricePerSqM)])
+    .range([height, 0]);
+  
+    
+  // Filter the data by the selected unit type
+//  const filteredData = data.filter(function(d) {
+//    return d.flat_type === unitType;
+//  });
+    
     const townCategory = {
   "Woodlands": "North",
   "Yishun": "North",
@@ -72,34 +107,35 @@ function updateGraph(unitType) {
 };
     
   // Group the data by town and calculate the average price per square metre
-  const townData = d3.group(filteredData, d => d.town);
-  const townPrices = Array.from(townData, ([town, data]) => {
-    const pricePerSqM = d3.mean(data, d => d["resale_price"] / d["floor_area_sqm"]);
-    return { town, pricePerSqM };
-  });
+//  const townData = d3.group(filteredData, d => d.town);
+//  const townPrices = Array.from(townData, ([town, data]) => {
+//    const pricePerSqM = d3.mean(data, d => d["resale_price"] / d["floor_area_sqm"]);
+//    return { town, pricePerSqM };
+//  });
+//    
+//    console.log(townPrices);
 
   // Sort the data by price per square metre
-  townPrices.sort(function(a, b) {
-    return a.pricePerSqM - b.pricePerSqM;
-  });
+//  townPrices.sort(function(a, b) {
+//    return a.pricePerSqM - b.pricePerSqM;
+//  });
 
   // Select the SVG element and set its size
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+
   const svg = d3.select("#graph");
 svg.attr("width", width)
    .attr("height", height);
-    console.log("test");
+//    console.log("test");
     
 
   // Set the scales for the x and y axes
-  const xScale = d3.scaleBand()
-    .domain(townPrices.map(d => d.town))
-    .range([0, width])
-    .padding(0.1);
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(townPrices, d => d.pricePerSqM)])
-    .range([height, 0]);
+//  const xScale = d3.scaleBand()
+//    .domain(townPrices.map(d => d.town))
+//    .range([0, width])
+//    .padding(0.1);
+//  const yScale = d3.scaleLinear()
+//    .domain([0, d3.max(townPrices, d => d.pricePerSqM)])
+//    .range([height, 0]);
 
     // Define a color scale for each category
 const colorScale = d3.scaleOrdinal()
@@ -124,23 +160,40 @@ bars.enter().append("rect")
 
   // Add the x-axis
   svg.append("g")
-    .attr("transform", `translate(0,${height})`)
+    .attr("transform", `translate(0,620)`)
+      .style("font-size", "8px")
     .call(d3.axisBottom(xScale));
 
+    svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height - 6)
+    .text("Towns in Singapore");
+    
+    
   // Add the y-axis
   svg.append("g")
     .call(d3.axisLeft(yScale));
+    
+    svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text(`Average price of ${unitType}`);
 
   // Add a title to the graph
-  svg.append("text")
-    .attr("x", width / 2)
-    .attr("y", 0 - margin.top / 2)
-    .attr("text-anchor", "middle")
-    .style("font-size", "16px")
-    .text(`Cheapest towns for ${unitType} units`);
+//  svg.append("text")
+//    .attr("x", width / 2)
+//    .attr("y", 40)
+//    .attr("text-anchor", "middle")
+//    .style("font-size", "36px")
+//    .text(`Average prices across different towns in Singapore for ${unitType}`);
 }
 
 // Initialize the graph with the default unit
-const defaultUnitType = "1 ROOM";
+const defaultUnitType = "2 ROOM";
 updateGraph(defaultUnitType);
 })
